dbff4e02f747ea843f4cad09cd10d77148261f76
Erick Bhrener <erick.silva@semace.ce.gov.br>
2021-08-18 21:42:37 -0300

    First commit

---
+++ src/main/java/com/example/craftbeer/CraftBeersApplication.java
@@ -0,0 +1,13 @@
+package com.example.craftbeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftBeersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftBeersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/craftbeer/CraftBeersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.craftbeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftBeersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
